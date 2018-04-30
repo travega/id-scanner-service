b1c222134de4643c213f41fe5049c7fb4ba270ac
Andrew Scott <andrewscott@andrewsc-ltm.internal.salesforce.com>
2018-04-30 23:31:56 +0100

    create new identified person record

--- app.rb
+++ app.rb
@@ -17,7 +17,27 @@ class App < Sinatra::Base
             return {error: "Error parsing JSON."}.to_json
         end
 
-        puts params["name"]
+        fname = params["fname"]
+        lname = params["lname"]
+        gender = params["gender"]
+        dob = params["dob"]
+        travel_doc_type = params["travelDocType"]
+        nationality = params["nationality"]
+        doc_code = params["docCode"]
+
+        payload = {
+            name: "#{fname} #{lname}",
+            first_name__c: fname,
+            last_name__c: lname,
+            gender__c: gender,
+            date_of_birth__c: dob,
+            travel_doc_type__c: travel_doc_type,
+            nationality__c: nationality,
+            document_code__c: doc_code,
+            external_id__c: SecureRandom.hex(12)
+        }
+
+        IdentifiedPerson.create!(payload);
 
         IdentifiedPerson.all.each do |identified_person|
             puts identified_person.name
